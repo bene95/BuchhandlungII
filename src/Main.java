4daf606364d6384479b4b64d5c8d208d794ac38f
Robert Hottmann <rob.hottmann.15@lehre.dhbw.mosbach.de>
2017-04-09 22:28:28 +0200

    Fertigen AES code gefunden und eingefügt, muss ihn noch auf die DB anpassen- lass dich nicht durch die Kommentare verwirren in der Main :P

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 package src;
 
-import Model.Book;
-
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        /*
         HSQLDBManager.instance.init();
         System.out.println(Configuration.instance.userDirectory);
         Book book = new Book("Metro2033","2","1");
@@ -23,6 +20,31 @@ public class Main {
         System.out.println(books.get(0).getTitel());
         System.out.println(books.get(1).getTitel());
         System.out.println(books.get(2).getTitel());
+               byte[] encryptionKey = "MZygpewJsCpRrfOr".getBytes(StandardCharsets.UTF_8);
+        byte[] plainText = "Hello world!".getBytes(StandardCharsets.UTF_8);
+        AdvancedEncryptionStandard advancedEncryptionStandard = new AdvancedEncryptionStandard(
+                encryptionKey);
+        byte[] cipherText = new byte[0];
+
+
+        try {
+            cipherText = advancedEncryptionStandard.encrypt(plainText);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        byte[] decryptedCipherText = new byte[0];
+
+        try {
+            decryptedCipherText = advancedEncryptionStandard.decrypt(cipherText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(new String(plainText));
+        System.out.println(new String(cipherText));
+        System.out.println(new String(decryptedCipherText));
+        */
 
 
         //Buchhandlung buchhandlung = new Buchhandlung();
