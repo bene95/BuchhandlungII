558ef5a64c75ae41f91b8f46d75e7f631bdc11cf
Benedikt Schack <ben.schack.15@lehre.mosbach.dhbw.de>
2017-04-09 13:43:14 +0200

    GUI erweitert

--- View/Buchhandlung.java
+++ View/Buchhandlung.java
@@ -2,26 +2,26 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import Model.Book;
 import src.*;
 
 
 public class Buchhandlung extends JFrame {
-    private JTabbedPane NewBook;
+    private JTabbedPane SearchBook;
     private JTextField txtNewBookTitle;
     private JTextField txtNewBookQuantity;
     private JTextField txtNewBookUUID;
     private JButton NewBookSubmit;
     private JButton btnUpdateBookSubmit;
-    private JTextField txtUpdateBookTitle;
-    private JLabel lblUpdateBookQuantity;
-    private JLabel lblUpdateBookUUID;
+    private JTextField txtUpdateBookOldTitle;
+    private JLabel lblUpdateBookNewTitle;
     private JLabel lblUpdteBookTitle;
-    private JTextField txtUpdateBookQuantity;
+    private JTextField txtUpdateBookNewTitle;
     private JTextField txtUpdateBookUUID;
     private JLabel lblNewBookTitle;
-    private JLabel lblNewBookUUID;
-    private JLabel lblNewBookQuantity;
     private JPanel tabNewBook;
     private JPanel tabUpdateBook;
     private JTextField txtDeleteBookTitle;
@@ -29,6 +29,16 @@ public class Buchhandlung extends JFrame {
     private JButton btnDeleteBookSubmit;
     private JLabel lblDeleteBookTitle;
     private JPanel rootPanel;
+    private JList lstSearchBook;
+    private JLabel lblSearchBookTitle;
+    private JTextField txtSearchBook;
+    private JButton btnSearchBookSubmit;
+    private JLabel lblSellBookTitle;
+    private JTextField txtSellBookTitle;
+    private JLabel lblSellTitleQuantity;
+    private JTextField txtSellBookQuantity;
+    private JButton btnSellBookSubmit;
+    private JButton undoButton;
 
 
     public Buchhandlung() {
@@ -36,7 +46,7 @@ public class Buchhandlung extends JFrame {
         pack();
         setContentPane(rootPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(new Dimension(400,275));
+        setSize(new Dimension(600,300));
 
         setVisible(true);
 
@@ -45,11 +55,29 @@ public class Buchhandlung extends JFrame {
             Configuration.instance.viewModel.createNewBook(book);
         });
         btnUpdateBookSubmit.addActionListener(actionEvent -> {
-            Book book = new Book(txtUpdateBookTitle.toString(),txtUpdateBookQuantity.toString(),txtUpdateBookUUID.toString());
+            Book book = new Book(txtUpdateBookOldTitle.toString(), txtUpdateBookNewTitle.toString(),txtUpdateBookUUID.toString());
             Configuration.instance.viewModel.updateBook(book);
         });
         btnDeleteBookSubmit.addActionListener(actionEvent -> {
             Configuration.instance.viewModel.deleteBook(txtDeleteBookTitle.toString());
+        });
+        btnSearchBookSubmit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        btnSellBookSubmit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        undoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                
+            }
         });
     }
 }
